cc7f97db1a17a4bc7e54dd527f6b1f0f7676218f
Ebrutokgoz <ebrutz44@gmail.com>
2026-01-06 17:25:02 +0300

    feat: setup onion architecture project structure

---
+++ Core/MovieApi.Application/Features/CqrsDesignPattern/Results/CategoryResults/GetCategoryQueryResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.CqrsDesignPattern.Results.CategoryResults
+{
+    internal class GetCategoryQueryResult
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=MovieApi;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;initial Catalog=ApiMovieDb;integrated Security=true");
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
