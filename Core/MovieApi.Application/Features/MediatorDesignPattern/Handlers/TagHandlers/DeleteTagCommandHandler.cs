9b5fa36264d31402fa0a1d6551494c4fa4ed3e4c
Ebrutokgoz <ebrutz44@gmail.com>
2026-01-12 15:37:41 +0300

    feat: add Mediator functions for Tags entity

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/TagHandlers/DeleteTagCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Commands.TagCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers
+{
+    public class DeleteTagCommandHandler : IRequestHandler<DeleteTagCommand>
+    {
+        private readonly MovieContext _context;
+
+        public DeleteTagCommandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(DeleteTagCommand request, CancellationToken cancellationToken)
+        {
+            var tag = await _context.Tags.FindAsync(request.Id);
+            if (tag != null)
+            {
+                _context.Tags.Remove(tag);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/TagHandlers/GetTagQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.TagResults;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers
+{
+    public class GetTagQueryHandler : IRequestHandler<GetTagQuery, List<GetTagQueryResult>>
+    {
+        private readonly MovieContext _context;
+
+        public GetTagQueryHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetTagQueryResult>> Handle(GetTagQuery request, CancellationToken cancellationToken)
+        {
+            var tagList = await _context.Tags.AsNoTracking().ToListAsync();
+            return tagList.Select(tag => new GetTagQueryResult
+            {
+                Id = tag.Id,
+                Title = tag.Title
+            }).ToList();
+        }
+    }
+}
